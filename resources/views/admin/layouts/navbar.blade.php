5240d1ad57ef04f5d5fd957ebb3e960c0b968f84
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2024-10-06 22:23:00 +0500

    edition

---
+++ resources/views/admin/layouts/navbar.blade.php
@@ -0,0 +1,50 @@
+<div class="sidebar-wrapper" data-simplebar="init"><div class="simplebar-wrapper" style="margin: 0px;"><div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div><div class="simplebar-mask"><div class="simplebar-offset" style="right: 0px; bottom: 0px;"><div class="simplebar-content-wrapper" style="height: 100%; overflow: hidden scroll;"><div class="simplebar-content mm-active" style="padding: 0px;">
+                        <div class="sidebar-header">
+                            <div>
+                                <img src="{{asset('admin/assets/images/logo-icon.png')}}" class="logo-icon" alt="logo icon">
+                            </div>
+                            <div>
+                                <h4 class="logo-text">GreenSpace</h4>
+                            </div>
+                            <div class="toggle-icon ms-auto"><i class="bx bx-arrow-to-left"></i>
+                            </div>
+                        </div>
+                        <!--navigation-->
+                        <ul class="metismenu mm-show" id="menu">
+                            <li class="mm-active">
+                                <a href="widgets.html">
+                                    <div class="parent-icon"><i class="bx bx-cookie"></i>
+                                    </div>
+                                    <div class="menu-title">Dashboard</div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="widgets.html">
+                                    <div class="parent-icon"><i class="bx bx-cookie"></i>
+                                    </div>
+                                    <div class="menu-title">Country</div>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="widgets.html">
+                                    <div class="parent-icon"><i class="bx bx-cookie"></i>
+                                    </div>
+                                    <div class="menu-title">City</div>
+                                </a>
+                            </li>
+
+                        </ul>
+                        <!--end navigation-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="simplebar-placeholder" style="width: auto; height: 1331px;"></div>
+    </div>
+    <div class="simplebar-track simplebar-horizontal" style="visibility: hidden;">
+        <div class="simplebar-scrollbar" style="width: 0px; display: none;"></div>
+    </div>
+    <div class="simplebar-track simplebar-vertical" style="visibility: visible;">
+        <div class="simplebar-scrollbar" style="height: 720px; transform: translate3d(0px, 0px, 0px); display: block;"></div>
+    </div>
+</div>

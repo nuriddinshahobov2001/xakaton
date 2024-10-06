16030bca277df1c4c23e84c826331937592b8aaf
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2024-10-06 23:09:32 +0500

    asdf

--- app/Models/AppModel.php
+++ app/Models/AppModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ApplicationOptionModel extends Model
+class AppModel extends Model
 {
     use HasFactory;
     protected $guarded = false;

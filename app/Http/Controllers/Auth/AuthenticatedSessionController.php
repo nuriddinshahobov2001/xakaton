c11de220cee87da1566a14b722cfc15ebb5e7d62
Nuriddin Shahobov <100368507+nuriddinshahobov2001@users.noreply.github.com>
2024-10-07 19:38:04 +0500

    asdf

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
         if (auth()->user()->role == 0) {
-            return redirect()->route('dashboard');
+            return redirect()->route('dash.index');
         }else {
             return redirect()->intended(RouteServiceProvider::HOME);
         }
